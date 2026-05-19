7e8167a02a3cb8ed97b4f4cc800f4db20373fafb
YYQ941026 <1539729795@qq.com>
2026-05-19 15:00:09 +0800

    关键字眼修改

--- apps/web-antd/src/upload-tip.ts
+++ apps/web-antd/src/upload-tip.ts
@@ -9,17 +9,17 @@ export function useUploadTip() {
     if (readTip.value || !import.meta.env.DEV) {
       return;
     }
-    const modalInstance = Modal.info({
-      title: '提示',
-      centered: true,
-      content:
-        '如果你的版本是从低版本升级到后端>5.4.0, 记得执行升级sql, 否则跳转页面(如oss 代码生成配置)等会404',
-      okButtonProps: { disabled: true },
-      onOk() {
-        modalInstance.destroy();
-        readTip.value = true;
-      },
-    });
+    // const modalInstance = Modal.info({
+    //   title: '提示',
+    //   centered: true,
+    //   content:
+    //     '如果你的版本是从低版本升级到后端>5.4.0, 记得执行升级sql, 否则跳转页面(如oss 代码生成配置)等会404',
+    //   okButtonProps: { disabled: true },
+    //   onOk() {
+    //     modalInstance.destroy();
+    //     readTip.value = true;
+    //   },
+    // });
 
     let time = 3;
     const interval = setInterval(() => {
